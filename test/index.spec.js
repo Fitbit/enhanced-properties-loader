772244f01bfaa17a7aca45eb75f9e1d6f758c412
Marat Dreizin <marat.dreizin@gmail.com>
2016-06-16 18:37:56 +0300

    tests: adds more tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,15 +4,16 @@ import {
 import loader from '../src'
 
 describe('enhanced-properties-loader', () => {
-    const makeRequest = (resourcePath, callback, options = {}) => {
+    const makeRequest = (resourcePath, callback, options = {}, query = null) => {
         loader.call({
             async: () => callback,
             resourcePath,
-            options: options
+            options,
+            query: query ? `?${JSON.stringify(query)}` : ''
         }, readFileSync(resourcePath, { encoding: 'utf8' }));
     };
 
-    it('should load `*.properties` successfully', done => {
+    it('should load `*.properties` with `namespaces`', done => {
         makeRequest('./test/fixtures/index.properties', (err, properties) => {
             expect(err).toBe(null);
             expect(properties).toEqual({
@@ -23,6 +24,20 @@ describe('enhanced-properties-loader', () => {
             });
 
             done();
+        });
+    });
+
+    it('should load `*.properties` without `namespaces`', done => {
+        makeRequest('./test/fixtures/index.properties', (err, properties) => {
+            expect(err).toBe(null);
+            expect(properties).toEqual({
+                foo: 1,
+                'bar.bar1': true
+            });
+
+            done();
+        }, {}, {
+            namespaces: false
         });
     });
 });
